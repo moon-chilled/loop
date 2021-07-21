db5817752928952daa621d6a52e19a801447396c
Moonchild <elronnd@elronnd.net>
2021-07-20 22:06:33 -0700

    conglomerate

--- loop.scm
+++ loop.scm
@@ -205,6 +205,124 @@
   (apply ((*classes* what) 'make) p))
 
 (define (type? var type) (member type ((*classes* (var 'class-name)) 'classes)))
+(define *loop-errors* (make-hash-table 8 eq?))
+
+(define-macro (add-error-formatter name parameter-list . body)
+  `(set! (*loop-errors* ',name)
+         (lambda*
+           (stream ,@(map (lambda (x) (if (pair? x) x `(,x (error "No argument supplied for parameter ~a" ',x))))
+                          parameter-list))
+           ,@body)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Condition reporters for parse errors.
+
+(add-error-formatter expected-var-spec-but-end ()
+  (format stream
+          "Expected a variable specification, but reached the end of the loop body."))
+
+(add-error-formatter expected-var-spec-but-found (found)
+  (format stream
+          "Expected a variable specification but found the following instead: ~s"
+          found))
+
+(add-error-formatter expected-simple-var-but-end ()
+  (format stream
+          "Expected a simple variable but reached the end of the loop body."))
+
+(add-error-formatter expected-simple-var-but-found (found)
+  (format stream
+          "Expected a simple variable but found the following instead: ~s"
+          found))
+
+(add-error-formatter expected-type-spec-but-end ()
+  (format stream
+          "Expected a variable specification but reached the end of the loop body."))
+
+(add-error-formatter expected-type-spec-but-found (found)
+  (format stream
+          "Expected a type specification but found the following instead: ~s"
+          found))
+
+(add-error-formatter expected-compound-form-but-end ()
+  (format stream
+          "Expected a compound form but reached the end of the loop body."))
+
+(add-error-formatter expected-compound-form-but-found (found)
+  (format stream
+          "Expected a compound form but found the following instead: ~s"
+          found))
+
+(add-error-formatter expected-form-but-end ()
+  (format stream
+          "Expected a form but reached the end of the loop body."))
+
+(add-error-formatter expected-symbol-but-end ()
+  (format stream
+          "Expected a symbol but reached the end of the loop body."))
+
+(add-error-formatter expected-symbol-but-found (found)
+  (format stream
+          "Expected a symbol but found the following instead: ~s"
+          found))
+
+(add-error-formatter expected-keyword-but-found (found)
+  (format stream
+          "Expected a loop keyword, but found the following instead: ~s"
+          found))
+
+(add-error-formatter expected-for/as-subclause-but-end ()
+  (format stream
+          "Expected a loop keyword indicating a for/as subclause, but reached the end of the loop body."))
+
+(add-error-formatter expected-symbol-but-found (found)
+  (format stream
+          "Expected a loop keyword indicating a for/as subclause, but found the following instead: ~s"
+          found))
+
+(add-error-formatter expected-each/the-but-end ()
+  (format stream
+          "Expected the loop keyword each/the, but reached the end of the loop body."))
+
+(add-error-formatter expected-each/the-but-found (found)
+  (format stream
+          "Expected the loop keyword each/the, but found the following instead: ~s"
+          found))
+
+(add-error-formatter expected-in/of-but-end ()
+  (format stream
+          "Expected the loop keyword in/or, but reached the end of the loop body."))
+
+(add-error-formatter expected-in/of-but-found (found)
+  (format stream
+          "Expected the loop keyword in/or, but found the following instead: ~s"
+          found))
+
+(add-error-formatter conflicting-stepping-directions ()
+  (format stream
+          "Conflicting stepping directions."))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Condition reporters for syntax errors.
+
+(add-error-formatter name-clause-not-first ()
+  (format stream
+          "A NAME loop clause was found, but it was not the first clause."))
+
+(add-error-formatter multiple-name-clauses ()
+  (format stream
+          "Multiple NAME clauses where found."))
+
+(add-error-formatter invalid-clause-order ()
+  (format stream
+          "Invalid clause order.  Variable clauses must precede main clauses."))
+
+(add-error-formatter multiple-variable-occurrences (bound-variable)
+  (format stream
+          "Multiple occurrences of the following variable were found: ~s"
+          bound-variable))
 ;;; The purpose of this generic function is to generate a list of all
 ;;; bound variables in a clause.  The same variable occurs as many
 ;;; times in the list as the number of times it is bound in the
@@ -802,31 +920,40 @@
            ,@acc)
        ,(do-clauses all-clauses end-tag))))
 
+(define-macro (augment-error-with-nice-message . body)
+  `(catch #t (lambda () ,@body)
+          (lambda (err rest)
+            (if (,*loop-errors* err)
+              (error err (apply (,*loop-errors* err) #f rest))
+              (apply error err rest)))))
+
 (define (expand-body loop-body)
-  (if (every pair? loop-body)
-      (let ((tag (gensym)))
-        `(call-with-exit
-           (letrec ((,tag (lambda (return)
-                            ,@loop-body
-                            (,tag return))))
-             ,tag)))
-      (let ((clauses (parse-loop-body loop-body))
-            (end-tag (gensym)))
-        (analyze-clauses clauses)
-        (let-temporarily ((*loop-name* (if (type? (car clauses) 'name-clause)
-                                         ((car clauses) 'name)
-                                         #f))
-                          (*loop-return-sym* (gensym))
-                          (*accumulation-variable* (gensym))
-                          (*list-tail-accumulation-variable* (gensym))
-                          (*tail-variables* (make-hash-table 8 eq?)))
-          ; todo incorporate *loop-name* to allow named return
+  (augment-error-with-nice-message
+    (if (every pair? loop-body)
+        (let ((tag (gensym)))
           `(call-with-exit
-             (lambda (return)
+             (letrec ((,tag (lambda (return)
+                              ,@loop-body
+                              (,tag return))))
+               ,tag)))
+        (let ((clauses (parse-loop-body loop-body))
+              (end-tag (gensym)))
+          (analyze-clauses clauses)
+          (let-temporarily ((*loop-name* (if (type? (car clauses) 'name-clause)
+                                           ((car clauses) 'name)
+                                           #f))
+                            (*loop-return-sym* (gensym))
+                            (*accumulation-variable* (gensym))
+                            (*list-tail-accumulation-variable* (gensym))
+                            (*tail-variables* (make-hash-table 8 eq?)))
+            ; todo incorporate *loop-name* to allow named return
+            `(,augment-error-with-nice-message
                (call-with-exit
-                 (lambda (,*loop-return-sym*)
-                   (let ((loop-finish (macro () `(,',end-tag))))
-                     ,(expand-clauses clauses end-tag))))))))))
+                 (lambda (return)
+                   (call-with-exit
+                     (lambda (,*loop-return-sym*)
+                       (let ((loop-finish (macro () `(,',end-tag))))
+                         ,(expand-clauses clauses end-tag))))))))))))
 ;;; In the dictionary entry for LOOP, the HyperSpec says:
 ;;;
 ;;;   main-clause ::= unconditional |
